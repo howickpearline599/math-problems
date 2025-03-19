12d7e2ea5a6db086e7686eaa5f13ebc9ad719298
howickpearline599 <howickpearline599@gmail.com>
2025-03-19 05:39:59 +0000

    Add 13.go

---
+++ src/13.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42 // This is a comment
+	y := 13
+
+	// Calculate the sum of x and y
+	sum := x + y
+
+	fmt.Println("The sum of", x, "and", y, "is", sum)
+}
